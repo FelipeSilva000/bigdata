0c63f6c40d86f68b8c0061dcc2bd5e639b0c20ae
Felipe Silva <fellipecllash@gmail.com>
2025-02-26 17:55:34 -0300

    add

--- exercicio/exe005.py
+++ exercicio/exe005.py
@@ -1,4 +1,13 @@
 #exe005
 print("============= DESAFIO =============")
-print("")
+print("5. Faça um programa que leia um número inteiro e verifique se ele é maior que 10."
+      "Em caso afirmativo, exiba “Maior que 10”, caso contrário, “Não é maior que 10”.")
 print("===================================")
+
+num = int(input("digite um número: "))
+if num > 10:
+    print(f"{num} é maior que 10")
+elif num == 10:
+    print(f"{num} é igual a 10")
+else:
+    print(f"{num} é menor que 10")
---
+++ exercicio/exe006.py
@@ -0,0 +1,17 @@
+#exe006
+print("============= DESAFIO =============")
+print("""6. Crie um programa que pergunte ao usuário quantos valores ele deseja inserir."
+Em seguida, use um loop para ler essa quantidade de números e, ao final, exiba a
+média aritmética deles.""")
+print("===================================")
+
+num = int(input("Digite quantos valores voçê quer: "))
+total = 0
+cont = 0
+for c in range(num):
+    soma = (int(input("digite um número")))
+    total = (soma+total)
+    cont = cont+1
+    media = (total/cont)
+
+print(f"A media dos valores é igual a {media}")
